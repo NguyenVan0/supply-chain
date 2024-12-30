ee2dd46b0613d91023507f82300ab8bd68ee74a8
hai van <nguyenthihaivan339099@gmail.com>
2024-12-30 16:23:18 +0700

    language

--- src/Components/Cards/ProductCard.js
+++ src/Components/Cards/ProductCard.js
@@ -3,7 +3,7 @@ import Rating from '../Rating';
 
 const ProductCard = ({ product }) => {
   const imageUrl = product.item["image_url"]
-  ? `https://gateway.pinata.cloud/ipfs/${product.item["image_url"]}` : ''
+    ? `https://gateway.pinata.cloud/ipfs/${product.item["image_url"]}` : '';
 
   return (
     <div className="my-1">
@@ -12,41 +12,41 @@ const ProductCard = ({ product }) => {
           <img 
             src={imageUrl}
             width="100%"
-            alt="Product Image"
+            alt="Hình ảnh sản phẩm"
           />
         </div>
         <div className="col-12 col-md-8">
-          <span className="card-key">Id: </span>
+          <span className="card-key">ID: </span>
           <span className="card-value">{product.item["id"]}</span>
           <br />
           <span className="card-key">Tên: </span>
           <span className="card-value">{product.item["title"]}</span>
           <br />
-          <span className="card-key">Manufacturer: </span>
+          <span className="card-key">Nhà sản xuất: </span>
           <span className="card-value">{product.manufacturer["name"]}</span>
           <br />
           <span className='d-flex align-items-center'>
-            <span className="card-key">Rating: </span> &nbsp;
+            <span className="card-key">Đánh giá: </span> &nbsp;
             <Rating rating={product.item["rating"] / 20} editable={false} />
           </span>
           <br />
           <span className="d-flex justify-content-around">
             {product.manufacturer.isVerified ? (
               <span className="">
-                <span className="badge bg-success">Verified</span>
+                <span className="badge bg-success">Đã xác minh</span>
               </span>
             ) : (
               <span className="">
-                <span className="badge bg-warning">Not Verified</span>
+                <span className="badge bg-warning">Chưa xác minh</span>
               </span>
             )}
             {product.manufacturer.isRenewableUsed ? (
               <span className="">
-                <span className="badge bg-success">Eco Friendly</span>
+                <span className="badge bg-success">Thân thiện với môi trường</span>
               </span>
             ) : (
               <span className="">
-                <span className="badge bg-warning">Non Eco Friendly</span>
+                <span className="badge bg-warning">Không thân thiện với môi trường</span>
               </span>
             )}
           </span>
